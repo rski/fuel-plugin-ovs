886e4cfe065f1f8a890cf0f5ec0a1cbc8fbb516d
Romanos Skiadas <rski@intracom-telecom.com>
2016-11-16 09:31:39 +0200

    Don't install dpdk on the controller
    Don't install dpdk on controller even if install same ovs on
    controller is checked.
    
    Change-Id: I2cc25ccc670bae21122b856f4390d8f712e6597c

--- deployment_scripts/puppet/manifests/ovs-install.pp
+++ deployment_scripts/puppet/manifests/ovs-install.pp
@@ -4,14 +4,15 @@ $master_ip      = hiera('master_ip')
 $ovs_settings   = hiera('fuel-plugin-ovs')
 $dpdk           = hiera_hash('dpdk', {})
 
+$is_controller  = roles_include['primary-controller', 'controller']
 $install_nsh    = $ovs_settings['install_nsh']
-$install_dpdk   = $ovs_settings['install_dpdk']
+$install_dpdk   = $ovs_settings['install_dpdk'] and !$is_controller
 $install_on_controller = $ovs_settings['install_on_controller']
 $ovs_socket_mem = join(pick($dpdk['ovs_socket_mem'], []), ',')
 
 
 if $operatingsystem == 'Ubuntu' {
-  if (!roles_include(['primary-controller', 'controller'])) or $install_on_controller {
+  if !$is_controller or $install_on_controller {
       exec { 'install ovs_nsh_dpdk':
           command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-1.0/deployment_scripts/install.sh | bash -s ${master_ip} ${install_nsh} ${install_dpdk} ${ovs_socket_mem}",
           path    => '/usr/bin:/usr/sbin:/bin:/sbin',
